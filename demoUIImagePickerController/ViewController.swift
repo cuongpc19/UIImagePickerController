4918bd7400a63c321b16fa0ee1e9126b26ed05c1
cuongpc <cuongpc19@gmail.com>
2017-02-20 08:47:41 +0700

    Initial Commit

---
+++ demoUIImagePickerController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoUIImagePickerController
+//
+//  Created by AgribankCard on 2/20/17.
+//  Copyright © 2017 cuongpc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
